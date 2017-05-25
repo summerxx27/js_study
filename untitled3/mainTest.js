c83259b09b506544ec976947e5e151c0c00fc7e0
summerxx27 <summerxx@163.com>
2017-05-25 18:29:36 +0800

    update
    update

---
+++ untitled3/mainTest.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zjwang on 2017/5/25.
+ */
+
+function myFunction()
+{
+    // 在js文件中如何取到html中的元素 通过id
+    document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数, 在 js 文件中";
+}
